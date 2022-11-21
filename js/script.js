a6abeb6dbf9e07b77ce76720ee10ddb17da0f7d1
lauraadan <info@lauraadan.com>
2022-11-21 16:10:05 +0100

    finish project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var zero = 0;
+$(document).ready(function(){
+    $(window).on('scroll', function(){
+        $('.navbar').toggleClass('hide', $(window).scrollTop()>zero);
+        zero = $(window).scrollTop();
+    })
+})
